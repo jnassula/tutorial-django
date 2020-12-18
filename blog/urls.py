f105f08283acd129272e8b066136668e3b38eaf4
jnassula <jonatanassula@gmail.com>
2020-12-18 19:07:39 +0000

    corrigindo erro no urls.py e post_list

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
-    path("slug:slug>/", views.PostDetailView.as_view(), name="detail"),
+    path("slug:<slug>/", views.PostDetailView.as_view(), name="detail"),
 ]
